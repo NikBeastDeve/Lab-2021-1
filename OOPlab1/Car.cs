490bd738571854a7650df3d999216937e411f756
NikBeastDeve <57311072+NikBeastDeve@users.noreply.github.com>
2021-07-01 20:00:46 +0600

    Add files via upload

---
+++ OOPlab1/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace OOPlab1
+{
+    public class Car
+    {
+        public string Make;
+        public string Model;
+        public decimal Value;
+        public string Color;
+
+        public Car()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherCar = obj as Car;
+            if (otherCar == null)
+            {
+                return false;
+            }
+            return this.Make == otherCar.Make && this.Model == otherCar.Model;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.Make + " " + this.Model;
+        }
+    }
+}
---
+++ OOPlab1/Human.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPlab1 {
+    public class Human {
+        public string FirstName;
+        public string LastName;
+        public Animal Pet;
+        public Phone Phone;
+        public int Number;
+        decimal _salary;
+        private Car _car;
+
+        public decimal Salary
+        {
+            get => _salary;
+            set
+            {
+                if (value > 0)
+                {
+                    Console.WriteLine("salary is added: " + value);
+                    _salary = value;
+                }
+            }
+        }
+
+        public Car car
+        {
+            get => _car;
+            set
+            {
+                if (value.Value < Salary)
+                {
+                    Console.WriteLine("car is added: " + value.ToString());
+                    _car = value;
+                }
+                else
+                {
+                    Console.WriteLine("car is added due to insufficient funds ");
+                }
+            }
+        }
+    }
+}
